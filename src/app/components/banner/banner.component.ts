6a4b7b45273cafb6a5aa4296ddc6f1dc59f693e1
Wanderlee Max <wanderleedev@gmail.com>
2023-09-26 12:54:14 -0500

    Edición del banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './banner.component.html',
 })
 export class BannerComponent {
-
+  btnBase = 'inline-flex gap-2 justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg inline-flex gap-2 justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg hover:scale-105 active:scale-95 duration-200 border-purple-500 border-[1px] shadow-md shadow-purple-800 hover:scale-105 active:scale-95 duration-200 border-purple-500 border-[1px] shadow-md shadow-purple-800'
 }
