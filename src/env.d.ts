6e1fcc87a1470055a6ad1c49bcd1ff7072682a63
wanderlee <wanderleedev@gmail.com>
2024-10-06 18:08:38 -0500

    [add] environments

---
+++ src/env.d.ts
@@ -0,0 +1,31 @@
+// Define the type of the environment variables.
+declare interface Env {
+  readonly NODE_ENV: string;
+  // Replace the following with your own environment variables.
+  // Example: NGX_VERSION: string;
+  [key: string]: any;
+  readonly NGX_EMAIL_JS_KEY: string
+  readonly NGX_EMAIL_JS_SERVICE_ID: string
+  readonly NGX_EMAIL_JS_TEMPLATE_ID: string
+  readonly NGX_CANONICAL_URL: string
+}
+
+// Choose how to access the environment variables.
+// Remove the unused options.
+
+// 1. Use import.meta.env.YOUR_ENV_VAR in your code. (conventional)
+declare interface ImportMeta {
+  readonly env: Env;
+}
+
+// 2. Use _NGX_ENV_.YOUR_ENV_VAR in your code. (customizable)
+// You can modify the name of the variable in angular.json.
+// ngxEnv: {
+//  define: '_NGX_ENV_',
+// }
+declare const _NGX_ENV_: Env;
+
+// 3. Use process.env.YOUR_ENV_VAR in your code. (deprecated)
+declare namespace NodeJS {
+  export interface ProcessEnv extends Env {}
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  EMAIL_JS_KEY: import.meta.env.NGX_EMAIL_JS_KEY,
+  EMAIL_JS_SERVICE_ID: import.meta.env.NGX_EMAIL_JS_SERVICE_ID,
+  EMAIL_JS_TEMPLATE_ID: import.meta.env.NGX_EMAIL_JS_TEMPLATE_ID,
+  CANONICAL_URL: import.meta.env.NGX_CANONICAL_URL,
+};
