5123fb9a55cfdc9d170589f4d54a7d9180e41049
wanderlee <wanderleedev@gmail.com>
2024-10-08 12:22:48 -0500

    [fix] routes assets

--- src/app/shared/components/noria/noria.component.ts
+++ src/app/shared/components/noria/noria.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class NoriaComponent {
   pathsImages = [
-    "/assets/images/angular-forms.webp",
-    "/assets/images/angular-httpModule.webp",
-    "/assets/images/angular-options.webp",
-    "/assets/images/angular-red.webp",
-    "/assets/images/angular-testing.webp",
-    "/assets/images/angularCli.webp",
-    "/assets/images/angularMaterial.webp",
-    "/assets/images/router.webp"
+    "assets/images/angular-forms.webp",
+    "assets/images/angular-httpModule.webp",
+    "assets/images/angular-options.webp",
+    "assets/images/angular-red.webp",
+    "assets/images/angular-testing.webp",
+    "assets/images/angularCli.webp",
+    "assets/images/angularMaterial.webp",
+    "assets/images/router.webp"
   ];
 
   public getImgCount (): number {
