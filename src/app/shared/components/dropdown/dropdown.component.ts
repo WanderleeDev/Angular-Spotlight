b1847afd5e9a0a92b12b6c4827879f34a7e42f1b
wanderlee <wanderleedev@gmail.com>
2024-10-08 22:28:56 -0500

    refactor contacts

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -9,12 +9,6 @@ import { Component, OnInit } from '@angular/core';
   }
   `,
 })
-export class DropdownComponent implements OnInit {
+export class DropdownComponent {
   currentLanguage = 'en';
-
-  ngOnInit(): void {
-    const params =new URL(window.location.href).pathname
-    console.log(params);
-
-  }
 }
