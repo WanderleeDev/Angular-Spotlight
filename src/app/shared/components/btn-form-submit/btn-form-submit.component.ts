7881138cdde3b94a6ed317b28aaecb7bb77bedda
wanderlee <wanderleedev@gmail.com>
2024-10-09 09:27:37 -0500

    [doc] fix translate

--- src/app/shared/components/btn-form-submit/btn-form-submit.component.ts
+++ src/app/shared/components/btn-form-submit/btn-form-submit.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-btn-form-submit',
   templateUrl: './btn-form-submit.component.html',
 })
-export class BtnFormSubmitComponent {
+export class BtnFormSubmitComponent implements OnChanges {
   @Input({ required: true }) isSubmitting = false;
   @Input({ required: true }) isDisabled!: boolean | null;
+  label: string = $localize`Send message`;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes['isSubmitting']?.currentValue);
+    if (changes['isSubmitting']) {
+      this.label = changes['isSubmitting'].currentValue
+        ? $localize`Sending...`
+        : $localize`Send message`;
+    }
+  }
 }
